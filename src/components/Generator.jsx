8881c0034d3e426b43e0b6faf63711590afae297
Sourabh <kanadesourabh420@gmail.com>
2021-09-29 18:56:12 +0530

    box-arranged

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,53 +1,47 @@
 import { useEffect, useState } from 'react';
-// import './index.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+// import SvgButton from './GenerateButt';
+
 
 function Generator() {
 const [temp, setTemp] = useState("");
 const [word, setWord] = useState("");
-const [size, setSize] = useState(400);
+const [size, setSize] = useState("");
 const [bgColor, setBgColor] = useState("ffffff");
 const [qrCode, setQrCode] = useState("");
 
-// Changing the URL only when the user
-// changes the input
+// Changing the URL only when the user changes the input
 useEffect(() => {
 	setQrCode
-(`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`);
+(`http://api.qrserver.com/v1/create-qr-code/?data=${word}&size=${size}x${size}&bgcolor=${bgColor}`);
 }, [word, size, bgColor]);
 
-// Updating the input word when user
-// click on the generate button
+// Updating the input word when user click on the generate button
 function handleClick() {
+	console.log('i am clicked');
 	setWord(temp);
 }
 
 return (
 	<div className="App">
 	<h1>QR Code Generator</h1>
-	<div className="input-box">
-		<div className="gen">
-		<input type="text" onChange={
-			(e) => {setTemp(e.target.value)}}
-			placeholder="Enter text to encode" />
-		<button className="button"
-			onClick={handleClick}>
-			Generate
-		</button>
+	<div className="input_box">
+		<div className="gen">	
+		<TextField label="Enter text to encode" type="text" required onChange={ (event) => {setTemp(event.target.value)}} placeholder="Input" />
+		<Button className="button" onClick={handleClick} variant="outlined" size="large"> Generate </Button>
 		</div>
-		<div className="extra">
+
+		{/* <div className="extra">
 		<h5>Background Color:</h5>
-		<input type="color" onChange={(e) =>
-		{ setBgColor(e.target.value.substring(1)) }} />
+		<input type="color" onChange={(event) => { setBgColor(event.target.value.substring(1)) }} />
 		<h5>Dimension:</h5>
-		<input type="range" min="200" max="600"
-		value={size} onChange={(e) =>
-		{setSize(e.target.value)}} />
-		</div>
+		<input type="range" min="100" max="500"	value={size} onChange={(event) =>	{setSize(event.target.value)}} />
+		</div> */}
 	</div>
 	<div className="output-box">
 		<img src={qrCode} alt="" />
-		<a href={qrCode} download="QRCode">
-		<button type="button">Download</button>
+		<a href={qrCode} download="QRCode">	<button type="button">Download</button>
 		</a>
 	</div>
 	</div>
